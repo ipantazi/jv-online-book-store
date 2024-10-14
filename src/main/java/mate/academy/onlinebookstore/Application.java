755d008439fe749b06d306bf220c77c280b9449c
Illya Pantazi <illios.piv@gmail.com>
2024-10-15 01:37:37 +0200

    Added to `application.properties` class - `spring.jpa.open-in-view=false` because was `WARN 7132 spring.jpa.open-in-view is enabled by default`. Added to `BookMapper` interface - `@Mapping(target = "id", ignore = true)` because was `[WARNING] Unmapped target property: "id"`.

--- src/main/java/mate/academy/onlinebookstore/Application.java
+++ src/main/java/mate/academy/onlinebookstore/Application.java
@@ -1,15 +1,10 @@
 package mate.academy.onlinebookstore;
 
-import mate.academy.onlinebookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    @Autowired
-    private BookService bookService;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/mate/academy/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/mate/academy/onlinebookstore/mapper/BookMapper.java
@@ -5,10 +5,12 @@ import mate.academy.onlinebookstore.dto.BookDto;
 import mate.academy.onlinebookstore.dto.CreateBookRequestDto;
 import mate.academy.onlinebookstore.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toBookDto(Book book);
 
+    @Mapping(target = "id", ignore = true)
     Book toModel(CreateBookRequestDto bookRequestDto);
 }
