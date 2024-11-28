5ffd5ffa6d5809af447a388c5b7bbaadd0d83330
Illya Pantazi <illios.piv@gmail.com>
2024-11-28 01:15:46 +0100

    Added `SpecificationProvider`, `SpecificationProviderManager` and `SpecificationBuilder` interfaces. Added new `AuthorSpecificationProvider`, `IsbnSpecificationProvider`, `PriceSpecificationProvider`, `TitleSpecificationProvider`, `BookSpecificationProviderManager`, `BookSpecificationBuilder` classes and `BookSearchParametersDto` record. Added `searchBooks()` method to `BookController` class, `search` method to `BookServiceImpl` class and to `BookService` interface. Extended `BookRepository` interface from `JpaSpecificationExecutor`.

--- src/main/java/mate/academy/onlinebookstore/repository/SpecificationBuilder.java
+++ src/main/java/mate/academy/onlinebookstore/repository/SpecificationBuilder.java
@@ -4,5 +4,5 @@ import mate.academy.onlinebookstore.dto.BookSearchParametersDto;
 import org.springframework.data.jpa.domain.Specification;
 
 public interface SpecificationBuilder<T> {
-    public Specification<T> build(BookSearchParametersDto searchParameters);
+    Specification<T> build(BookSearchParametersDto searchParameters);
 }
